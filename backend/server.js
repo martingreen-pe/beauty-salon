6afc914cd9403e30bb05660231c708f504c5e34f
martingreen-pe <martingreen.pe@gmail.com>
2024-09-20 18:19:40 -0500

    actualizando8

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');  // Asegúrate de incluir esto
+const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
@@ -10,8 +10,8 @@ app.use(express.json());
 // Habilitar CORS
 const allowedOrigins = [
   'http://localhost:3000', 
-  'http://192.168.0.104:3000',  // Tu IP local para acceso desde dispositivos móviles
-  'https://beauty-salon-77.onrender.com/'  // El frontend en producción
+  'http://192.168.0.104:3000',  // IP local para acceso desde dispositivos móviles
+  'https://beauty-salon-77.onrender.com'  // El frontend en producción
 ];
 
 // Configuración CORS
@@ -28,7 +28,10 @@ app.use(cors({
 }));
 
 // Conectar a MongoDB
-mongoose.connect('mongodb+srv://martinenriquepe:bwbEoDbX7KW5WzW5@beauty-salon.mswkl.mongodb.net/?retryWrites=true&w=majority&appName=beauty-salon')
+mongoose.connect('mongodb+srv://martinenriquepe:bwbEoDbX7KW5WzW5@beauty-salon.mswkl.mongodb.net/?retryWrites=true&w=majority&appName=beauty-salon', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('Conectado a MongoDB'))
   .catch((err) => console.log(err));
 
@@ -44,5 +47,5 @@ app.get('/*', (req, res) => {
 });
 
 // Puerto del servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;  // Render proporcionará el puerto
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
